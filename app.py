4b5444031e80acee34371827773086547a8167ec
Vinod VV <vinovijayan@gmail.com>
2025-07-18 05:22:58 +0530

    Enhanced to look more appealing

--- app.py
+++ app.py
@@ -65,5 +65,10 @@ def contact():
     return render_template('contact.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(debug=True)
